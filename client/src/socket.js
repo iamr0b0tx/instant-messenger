a34cb7f4278fc008be41b8a238a4af020ba28944
iamr0b0tx <abduladeneye@gmail.com>
2021-07-16 23:53:35 +0100

    secured socker based on socket.io tutorial

--- client/src/socket.js
+++ client/src/socket.js
@@ -9,6 +9,14 @@ import {
 // const socket = io(window.location.origin);
 const socket = io(window.location.origin);
 
+const sessionID = localStorage.getItem("sessionID");
+
+if (sessionID) {
+  this.usernameAlreadySelected = true;
+  socket.auth = { sessionID };
+  socket.connect();
+}
+
 // function destroyed() {
 //   socket.off("connect_error");
 // }
@@ -20,6 +28,17 @@ socket.on("connect", () => {
     if (err.message === "invalid username") {
       this.usernameAlreadySelected = false;
     }
+  });
+
+  socket.on("session", ({ sessionID, userID }) => {
+    // attach the session ID to the next reconnection attempts
+    socket.auth = { sessionID };
+
+    // store it in the localStorage
+    localStorage.setItem("sessionID", sessionID);
+
+    // save the ID of the user
+    socket.userID = userID;
   });
 
   socket.on("add-online-user", (id) => {
---
+++ server/sessionStore.js
@@ -0,0 +1,19 @@
+class InMemorySessionStore {
+  constructor() {
+    this.sessions = new Map();
+  }
+
+  findSession(id) {
+    return this.sessions.get(id);
+  }
+
+  saveSession(id, session) {
+    this.sessions.set(id, session);
+  }
+
+  findAllSessions() {
+    return [...this.sessions.values()];
+  }
+}
+
+export default InMemorySessionStore;
