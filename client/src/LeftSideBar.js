731d33fc370d57e3b493acc347dcb8b6007f3fee
iamr0b0tx <abduladeneye@gmail.com>
2021-07-21 23:29:14 +0100

    disabled the socket.to

--- client/src/LeftSideBar.js
+++ client/src/LeftSideBar.js
@@ -12,14 +12,14 @@ const LeftSideBar = (props) => {
   const classes = formStyles();
 
   return (
-      <Grid item xs={4} className={classes.leftSideBar}>
+      <Grid container item xs={4} className={classes.leftSideBar}>
         <Grid
             className="shade"
             container
             direction="row"
             justifyContent="center"
             alignItems="center">
-          <Grid container justifyContent="space-around" xs={9}>
+          <Grid container item justifyContent="space-around" xs={9}>
             <img
               className={classes.media}
               src={leftSideBarSVG}
--- client/src/socket.js
+++ client/src/socket.js
@@ -12,6 +12,13 @@ const socket = io(window.location.origin, {query: {token: token}});
 
 socket.on("connect", () => {
   console.log("connected to server");
+  // socket.on("session", ({ sessionID, userID }) => {
+  //   // attach the session ID to the next reconnection attempts
+  //   socket.auth = { sessionID };
+  //
+  //   // save the ID of the user
+  //   socket.userID = userID;
+  // });
 
   socket.on("add-online-user", (id) => {
     store.dispatch(addOnlineUser(id));
